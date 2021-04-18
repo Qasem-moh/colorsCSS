191a4f32105cd32bb7c85a81c137d5d473346607
Qasemmoh <>
2021-04-18 22:38:45 +0000

    add content home page

---
+++ js/index.js
@@ -0,0 +1,12 @@
+alert("h")
+var inputColor=document.getElementById("inputColor")
+var selectorColor=document.getElementById("selectorColor")
+// bodyPage.style.backgroundColorr=inputColor;
+function myFunction() {
+  console.log(inputColor)
+  document.getElementById("myDIV").style.backgroundColor = inputColor.value;
+}
+function myFunction1() {
+  console.log(selectorColor.value)
+  document.getElementById("myDIV").style.backgroundColor = selectorColor.value;
+}
