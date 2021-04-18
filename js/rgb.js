2a551eda988dbb22cd818e783a39a198596fc61d
Qasemmoh <>
2021-04-18 23:39:49 +0000

    r

---
+++ js/rgb.js
@@ -0,0 +1,9 @@
+alert("h")
+function setValueRange(){
+  var x=document.getElementById("rangData").value
+  document.getElementById("xin").innerText=x.value
+}
+
+function updateTextInput(val) {
+          document.getElementById('textInput').value=val; 
+        }
