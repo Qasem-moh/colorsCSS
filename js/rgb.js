316ae8bc1758d86a5603ac8e1147467e7d88a5b9
Qasemmoh <>
2021-04-19 10:27:23 +0000

    add js to rgb

--- js/rgb.js
+++
@@ -1,9 +0,0 @@
-alert("h")
-function setValueRange(){
-  var x=document.getElementById("rangData").value
-  document.getElementById("xin").innerText=x.value
-}
-
-function updateTextInput(val) {
-          document.getElementById('textInput').value=val; 
-        }
